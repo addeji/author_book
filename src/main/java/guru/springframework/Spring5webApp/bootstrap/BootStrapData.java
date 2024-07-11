6c7449251c3b2f475f201b05b84de950c814e00d
TAIWO-ADEDEJI <tadedeji@systemspecscomng.onmicrosoft.com>
2024-07-11 14:18:28 +0100

    first spring commit

---
+++ src/main/java/guru/springframework/Spring5webApp/bootstrap/BootStrapData.java
@@ -0,0 +1,106 @@
+package guru.springframework.Spring5webApp.bootstrap;
+
+import guru.springframework.Spring5webApp.domain.Author;
+import guru.springframework.Spring5webApp.domain.Book;
+import guru.springframework.Spring5webApp.domain.Publishers;
+import guru.springframework.Spring5webApp.repositories.AuthorRepository;
+import guru.springframework.Spring5webApp.repositories.BookRepository;
+
+import guru.springframework.Spring5webApp.repositories.PublisherRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
+
+
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository
+            , PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Started in BootStrap");
+        Publishers publishers = new Publishers();
+        publishers.setPublisherName("Epic");
+        publishers.setAddressline1("ikorodu,erunwen");
+        publishers.setCity("Lagos");
+        publishers.setZip("174333");
+        publisherRepository.save(publishers);
+        System.out.println(" name:" + publishers.getPublisherName());
+
+
+
+
+
+
+
+
+
+        Author eric = new Author("eric","aurthur");
+        Book Got = new Book("Book of Ice","2456876453");
+
+
+        eric.getBooks().add(Got);
+        Got.getAuthors().add(eric);
+        Got.setPublishers(publishers);
+        publishers.getBooks().add(Got);
+
+
+
+        authorRepository.save(eric);
+        bookRepository.save(Got);
+        publisherRepository.save(publishers);
+
+        Publishers publisher = new Publishers();
+        publisher.setPublisherName("Ea");
+        publisher.setAddressline1("ikeja,mainland ,lagos");
+        publisher.setCity("Lagos");
+        publisher.setZip("100001");
+        publisherRepository.save(publisher);
+        System.out.println(" name:" + publisher.getPublisherName());
+
+
+
+        Author rod = new Author("Rod","james");
+        Book ToG = new Book("Throne of Glass","1443876453");
+
+        rod.getBooks().add(ToG);
+        ToG.getAuthors().add(rod);
+        ToG.setPublishers(publisher);
+        publisher.getBooks().add(ToG);
+
+
+
+
+        authorRepository.save(rod);
+        bookRepository.save(ToG);
+        publisherRepository.save(publisher);
+        System.out.println(" books published:" + publishers.getBooks().size());
+        System.out.println(" books published:" + publisher.getBooks().size());
+
+
+
+        System.out.println("Total");
+        System.out.println("Number of Books :" + bookRepository.count());
+        System.out.println("Number of Authors :" + authorRepository.count());
+        System.out.println(" Number of Publishers:" + publisherRepository.count());
+
+
+
+
+
+    }
+
+}
---
+++ src/main/java/guru/springframework/Spring5webApp/domain/Publishers.java
@@ -0,0 +1,116 @@
+package guru.springframework.Spring5webApp.domain;
+
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+
+public class Publishers {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String PublisherName;
+    private String addressline1;
+    private String city;
+    private String zip;
+
+    @OneToMany
+    @JoinColumn(name = "publisher_id")
+    private Set<Book> books = new HashSet<>();
+
+
+
+
+    public Publishers() {
+    }
+
+    public Publishers(String publisherName, Long id, String addressline1, String city, String zip, Set<Book> books) {
+        PublisherName = publisherName;
+        this.id = id;
+        this.addressline1 = addressline1;
+        this.city = city;
+        this.zip = zip;
+        this.books = books;
+    }
+
+    public String getAddressline1() {
+        return addressline1;
+    }
+
+    public void setAddressline1(String addressline1) {
+        this.addressline1 = addressline1;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Publishers(String publisherName) {
+        PublisherName = publisherName;
+    }
+
+    public String getPublisherName() {
+        return PublisherName;
+    }
+
+    public void setPublisherName(String publisherName) {
+        PublisherName = publisherName;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Publishers{" +
+                "id=" + id +
+                ", PublisherName='" + PublisherName + '\'' +
+                ", addressline1='" + addressline1 + '\'' +
+                ", city='" + city + '\'' +
+                ", zip='" + zip + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Publishers publishers = (Publishers) o;
+        return id!=null ? id.equals(publishers.id) : publishers.id== null;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
